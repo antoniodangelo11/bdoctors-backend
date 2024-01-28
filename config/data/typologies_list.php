31ebd23ae1687b95f7b8865d1ada21420f8cb34b
l3on <l3onwebdev@gmail.com>
2024-01-28 17:14:32 +0100

    added typology resource

---
+++ config/data/typologies_list.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    "Cardiologia",
+    "Dermatologia",
+    "Endocrinologia",
+    "Gastroenterologia",
+    "Ematologia",
+    "Neurologia",
+    "Oncologia",
+    "Oftalmologia",
+    "Ortopedia",
+    "Otorinolaringoiatria",
+    "Pediatria",
+    "Psichiatria",
+    "Radiologia",
+    "Chirurgia generale",
+    "Chirurgia plastica",
+    "Chirurgia cardiaca",
+    "Chirurgia vascolare",
+    "Chirurgia ortopedica",
+    "Medicina interna",
+    "Medicina di emergenza"
+];
---
+++ database/seeders/TypologySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Typology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $typologies_list = config('data.typologies_list');
+        foreach ($typologies_list as $typology) {
+            $new_tipology = new Typology();
+
+            $new_tipology->name = $typology;
+
+            $new_tipology->save();
+        }
+    }
+}
