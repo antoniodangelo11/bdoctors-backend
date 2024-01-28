c7dc56ef8ac30629b2ae926027afe97be99f6233
l3on <l3onwebdev@gmail.com>
2024-01-28 20:58:08 +0100

    fixed header user name

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -50,7 +50,7 @@
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                                {{ Auth::user()->getFullName() }}
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
