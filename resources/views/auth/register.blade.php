d0279c085c2572e66d99b809af5a4a3c2b582b22
l3on <l3onwebdev@gmail.com>
2024-01-29 21:13:33 +0100

    minor fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -226,7 +226,7 @@
 
                             {{-- Actions --}}
                             <div class="mb-4 row mb-0">
-                                <div class="col-md-6 offset-md-4">
+                                <div class="col-md-6">
                                     <button type="submit" class="btn btn-primary">
                                         {{ __('Register') }}
                                     </button>
