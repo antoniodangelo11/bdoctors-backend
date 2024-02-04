65b1f7282fd2cc646b83180fe0e5d0f87548ac6d
l3on <l3onwebdev@gmail.com>
2024-02-04 22:34:51 +0100

    fixed photo url

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -33,6 +34,17 @@ class Profile extends Model
     public function typologies()
     {
         return $this->belongsToMany(Typology::class);
+    }
+
+    //*** GETTERS ***//
+    /**
+     * Get Photo absolute path on API
+     */
+    protected function photo(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => app('request')->is('api/*') && $value ? url('storage/' . $value) : $value,
+        );
     }
 
 
