83242688895f361d91edaeca0c69ef6eb45e9561
l3on <l3onwebdev@gmail.com>
2024-02-08 20:27:12 +0100

    added star resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypologySeeder::class,
+            StarSeeder::class,
             UserSeeder::class,
         ]);
     }
